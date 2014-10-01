5ff80e89bc81fefb9fd8f166a8258ca4864e3e7a
Brok9n <Brok9n@users.noreply.github.com>
2014-10-01 15:24:54 +1000

    updates

--- src/4917.cpp
+++ src/4917.cpp
@@ -3,13 +3,13 @@
 class cpu {
 	private:
 		char Memory[16];
-		char IP, IS, R0, R1;
+		char IP, IS[2], R0, R1;
 	public:
 		void insert(char ops[], int size);
 		void run(void);
 		
 		char get_ip() { return IP; };
-		char get_is() { return IS; };
+		char* get_is() { return IS; };
 		char get_r0() { return R0; };
 		char get_r1() { return R1; };
 		
@@ -29,16 +29,16 @@ void cpu::insert(char ops[], int size)
 
 void cpu::run(void)
 {
-	IP = 0, IS = 0, R0 = 0, R1 = 0;
+	IP = 0, IS[0] = 0, IS[1] = 0, R0 = 0, R1 = 0;
 	
 	int halt = 0;
 	
 	for (IP=0; IP<16 && !halt; IP++){
-		IS = Memory[(int)IP];
-		printf("IP = %d\n", IP);
-		printf("IS = %c\n", IS);
+		IS[0] = Memory[(int)IP];
+		//printf("IP = %d\n", IP);
+		//printf("IS = %c\n", IS[0]);
 		
-		switch (IS)
+		switch (IS[0])
 		{
 			case '0':
 			{
@@ -79,7 +79,22 @@ void cpu::run(void)
 			{
 				printf("BEEP!!!\n");
 				break;
-			}	
+			}
+			case '8':
+			{
+				IP++;
+				IS[1] = Memory[(int)IP];
+				//printf("IP = %d\n", IP);
+				//printf("IS = %c\n", IS[1]);
+				printf("%c\n", IS[1]);
+				
+				break;
+			}
+			case '9':
+			{
+				
+				break;
+			}
 		}
 	}
 }
@@ -87,9 +102,9 @@ void cpu::run(void)
 int main(void)
 {
 	cpu test;
-	char ops [] = {'7', '0'};
+	char ops [] = {'7', '8', '4', '0'};
 	
-	test.insert(ops, 2);
+	test.insert(ops, 4);
 	test.run();
 	
 	return 0;
